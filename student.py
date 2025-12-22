a4bd6f7002e4f445e96e384c75b6267de3c49839
Matteo Pelliccione <pelliccione.1871526@studenti.uniroma1.it>
2025-12-22 12:04:03 +0100

    Reverted back

--- student.py
+++ student.py
@@ -103,8 +103,7 @@ class Policy(nn.Module):
                 old_means.append(mean.squeeze(0).cpu().numpy())
                 old_log_stds.append(log_std.squeeze(0).detach().cpu().numpy())
                 
-                action_np = action.squeeze(0).cpu().numpy().astype(np.float32)
-                next_state, reward, terminated, truncated, _ = env.step(action_np)
+                next_state, reward, terminated, truncated, _ = env.step(action.squeeze(0).cpu().numpy())
                 done = terminated or truncated
                 rewards.append(reward)
                 dones.append(done)
